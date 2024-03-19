4b75f44a5ce89cc0b4215d88ab4b2c09968fb7b1
Seshat02 <jrcorpas02@gmail.com>
2024-03-19 12:41:32 -0400

    Asteroid raylib

---
+++ Ship-Game/Ship-Game/Asteroids.cs
@@ -0,0 +1,40 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+namespace Ship_Game
+{
+    internal class Asteroids
+    {
+        static string title = "Ship Game";
+
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(1620, 1080, title);
+            Raylib.SetTargetFPS(60);
+
+            Setup();
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+
+                Raylib.ClearBackground(Color.RayWhite);
+
+                Update();
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+        static void Setup()
+        {
+
+        }
+        static void Update()
+        {
+
+        }
+         
+    }
+}
