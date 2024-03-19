1684cf393a60c8414ce45978d22f9b2721d4502a
Wear shark <Wearshark13@Gmail.com>
2024-03-19 12:32:48 -0400

    add removed comments

--- Ship-Game/Ship-Game/Program.cs
+++ Ship-Game/Ship-Game/Program.cs
@@ -5,45 +5,45 @@ namespace ShipGame
 {
     internal class Program
     {
-        // If you need variables in the Program class (outside functions), you must mark them as static
+        
         static string title = "Ship Game";
 
         static void Main(string[] args)
         {
-            // Create a window to draw to. The arguments define width and height
+            
             Raylib.InitWindow(1620, 1080, title);
-            // Set the target frames-per-second (FPS)
+           
             Raylib.SetTargetFPS(60);
 
-            // Setup your game. This is a function YOU define.
+            
             Setup();
 
-            // Loop so long as window should not close
+            
             while (!Raylib.WindowShouldClose())
             {
-                // Enable drawing to the canvas (window)
+                
                 Raylib.BeginDrawing();
-                // Clear the canvas with one color
+                
                 Raylib.ClearBackground(Color.RayWhite);
 
-                // Your game code here. This is a function YOU define.
+                
                 Update();
 
-                // Stop drawing to the canvas, begin displaying the frame
+                
                 Raylib.EndDrawing();
             }
-            // Close the window
+            
             Raylib.CloseWindow();
         }
 
         static void Setup()
         {
-            // Your one-time setup code here
+            
         }
 
         static void Update()
         {
-            // Your game code run each frame here
+            
         }
     }
 }
