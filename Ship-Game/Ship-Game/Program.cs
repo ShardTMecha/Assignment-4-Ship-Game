5cee8120ffb075c3ce7be8c63ede4ff61b2d9264
Wear shark <Wearshark13@Gmail.com>
2024-03-19 11:52:35 -0400

    add raylib and template project

--- Ship-Game/Ship-Game/Program.cs
+++ Ship-Game/Ship-Game/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace Ship_Game
+﻿using Raylib_cs;
+using System.Numerics;
+
+namespace ShipGame
 {
     internal class Program
     {
+        // If you need variables in the Program class (outside functions), you must mark them as static
+        static string title = "Game Title";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create a window to draw to. The arguments define width and height
+            Raylib.InitWindow(800, 600, title);
+            // Set the target frames-per-second (FPS)
+            Raylib.SetTargetFPS(60);
+
+            // Setup your game. This is a function YOU define.
+            Setup();
+
+            // Loop so long as window should not close
+            while (!Raylib.WindowShouldClose())
+            {
+                // Enable drawing to the canvas (window)
+                Raylib.BeginDrawing();
+                // Clear the canvas with one color
+                Raylib.ClearBackground(Color.RayWhite);
+
+                // Your game code here. This is a function YOU define.
+                Update();
+
+                // Stop drawing to the canvas, begin displaying the frame
+                Raylib.EndDrawing();
+            }
+            // Close the window
+            Raylib.CloseWindow();
+        }
+
+        static void Setup()
+        {
+            // Your one-time setup code here
+        }
+
+        static void Update()
+        {
+            // Your game code run each frame here
         }
     }
 }
