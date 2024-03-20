949c23ae6a48f8a9a13764770b8001356094cf57
Seshat02 <jrcorpas02@gmail.com>
2024-03-20 14:04:27 -0400

    Random and variables

--- Ship-Game/Ship-Game/Asteroids.cs
+++ Ship-Game/Ship-Game/Asteroids.cs
@@ -7,6 +7,23 @@ namespace Ship_Game
     {
         static string title = "Ship Game";
 
+        static Random rng = new Random();
+
+        //General values
+        Vector2 asteroidVelocity;
+        Vector2 initialPosition;
+        Color color;
+
+        //Big asteroid values
+        Vector2 asteroidPosition1;
+        int asteroidRadius1;
+        int bigAsteroidAmount;
+
+        //Small asteroid values
+        Vector2 asteroidPosition2;
+        int asteroidRadius2;
+        int smallAsteroidAmount;
+
         static void Main(string[] args)
         {
             Raylib.InitWindow(1620, 1080, title);
@@ -35,6 +52,28 @@ namespace Ship_Game
         {
 
         }
-         
+        //Random position generator
+        public void Random()
+        {
+            //Random Y for big asteroids
+            asteroidPosition1 = new Vector2(800, (rng.Next(Raylib.GetScreenWidth(), Raylib.GetScreenHeight()) * 50));
+            //Random Y for small asteroids
+            asteroidPosition2 = new Vector2(800, (rng.Next(Raylib.GetScreenWidth(), Raylib.GetScreenHeight()) * 50));
+        }
+
+        //Check collision between player and asteroids       
+        public void Collision()
+        {
+
+        }
+        public void DrawAsteroids()
+        {
+            //Big asteroid Drawing
+            Raylib.DrawCircleV(asteroidPosition1, asteroidRadius1, color);
+
+            //Small asteroid Drawing
+            Raylib.DrawCircleV(asteroidPosition1, asteroidRadius1, color);
+        }
+
     }
 }
