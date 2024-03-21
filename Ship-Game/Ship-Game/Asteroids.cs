f785bab43431dd610cbffd68480aa3e3e17913ff
Seshat02 <jrcorpas02@gmail.com>
2024-03-21 11:55:08 -0400

    Variables Update2

--- Ship-Game/Ship-Game/Asteroids.cs
+++ Ship-Game/Ship-Game/Asteroids.cs
@@ -1,79 +1,48 @@
 ﻿using Raylib_cs;
 using System.Numerics;
+internal class Asteroids
+{  
+    static Random rng = new Random();
 
-namespace Ship_Game
-{
-    internal class Asteroids
+    //General values
+    Vector2 asteroidVelocity;
+    Vector2 initialPosition;
+    Color color = Color.Black;
+
+    //Big asteroid values
+    Vector2 asteroidPosition1;
+    int asteroidRadius1 = 25;
+    int bigAsteroidAmount;
+
+    //Small asteroid values
+    Vector2 asteroidPosition2;
+    int asteroidRadius2 = 15;
+    int smallAsteroidAmount;
+   
+    ////Random position generator
+    //public void Random()
+    //{
+        
+        
+    //}
+
+    //Check collision between player and asteroids       
+   
+    public void DrawAsteroids()
     {
-        static string title = "Ship Game";
+        asteroidRadius1 = 25;
+        asteroidRadius2 = 15;
+        //Random Y for big asteroids
+        asteroidPosition1 = new Vector2(800, (rng.Next(Raylib.GetScreenHeight(), Raylib.GetScreenWidth()) * 50));
+        //Random Y for small asteroids
+        asteroidPosition2 = new Vector2(800, (rng.Next(Raylib.GetScreenHeight(), Raylib.GetScreenWidth()) * 50));
 
-        static Random rng = new Random();
+        //Big asteroid Drawing
+        Raylib.DrawCircleV(asteroidPosition1, asteroidRadius1, color);
 
-        //General values
-        Vector2 asteroidVelocity;
-        Vector2 initialPosition;
-        Color color;
-
-        //Big asteroid values
-        Vector2 asteroidPosition1;
-        int asteroidRadius1;
-        int bigAsteroidAmount;
-
-        //Small asteroid values
-        Vector2 asteroidPosition2;
-        int asteroidRadius2;
-        int smallAsteroidAmount;
-
-        static void Main(string[] args)
-        {
-            Raylib.InitWindow(1620, 1080, title);
-            Raylib.SetTargetFPS(60);
-
-            Setup();
-
-            while (!Raylib.WindowShouldClose())
-            {
-                Raylib.BeginDrawing();
-
-                Raylib.ClearBackground(Color.RayWhite);
-
-                Update();
-
-                Raylib.EndDrawing();
-            }
-
-            Raylib.CloseWindow();
-        }
-        static void Setup()
-        {
-
-        }
-        static void Update()
-        {
-
-        }
-        //Random position generator
-        public void Random()
-        {
-            //Random Y for big asteroids
-            asteroidPosition1 = new Vector2(800, (rng.Next(Raylib.GetScreenWidth(), Raylib.GetScreenHeight()) * 50));
-            //Random Y for small asteroids
-            asteroidPosition2 = new Vector2(800, (rng.Next(Raylib.GetScreenWidth(), Raylib.GetScreenHeight()) * 50));
-        }
-
-        //Check collision between player and asteroids       
-        public void Collision()
-        {
-
-        }
-        public void DrawAsteroids()
-        {
-            //Big asteroid Drawing
-            Raylib.DrawCircleV(asteroidPosition1, asteroidRadius1, color);
-
-            //Small asteroid Drawing
-            Raylib.DrawCircleV(asteroidPosition1, asteroidRadius1, color);
-        }
+        //Small asteroid Drawing
+        Raylib.DrawCircleV(asteroidPosition2, asteroidRadius2, color);
+    }
     public void Collision()
     {
         //Check collision with big asteroids
@@ -84,4 +53,5 @@ namespace Ship_Game
         //float radii2 = asteroidRadius2 + play.radius;
         //bool overlapSmallAst = Vector2.Distance(asteroidPosition2, players stats) <= radii2;
     }
+
 }
--- Ship-Game/Ship-Game/Program.cs
+++ Ship-Game/Ship-Game/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace Ship_Game
+﻿using Raylib_cs;
+using System.Numerics;
+
+namespace Ship_Game
 {
     internal class Program
     {
+        static string title = "Ship Game";
+
+        static Asteroids[] asteroids;
+        static int numAsteroidsSpawning = 5;
+        static int asteroidount = 0;
+        static int maxNumAsteroids = 50;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Raylib.InitWindow(1620, 1080, title);
+            Raylib.SetTargetFPS(60);
+
+            Setup();
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+
+                Raylib.ClearBackground(Color.RayWhite);
+
+                Update();
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
         }
+        static void Setup()
+        {
+            asteroids = new Asteroids[maxNumAsteroids];
+
+            for (int i = 0; i <= numAsteroidsSpawning; i++)
+            {
+                asteroids[i] = new Asteroids();
+                asteroidount++;
+            }
+        }
+        static void Update()
+        {
+            foreach (Asteroids asteroid in asteroids)
+            {
+                asteroid.DrawAsteroids();
+            }
+        }
+       
+
     }
 }
