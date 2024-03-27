87e5776674d772d5d324e7e4d8870fffc598f67a
Seshat02 <jrcorpas02@gmail.com>
2024-03-26 23:20:29 -0400

    Add Raylib

--- Ship-Game/Ship-Game/Program.cs
+++ Ship-Game/Ship-Game/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace Ship_Game
+﻿using Raylib_cs;
+using System.Numerics;
+
+namespace Ship_Game
 {
     internal class Program
     {
+        static string title = "Ship Game";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Raylib.InitWindow(1920,1100,title);
+            Raylib.SetTargetFPS(60);
+
+            Setup();
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.RayWhite);
+
+                Update();
+
+                Raylib.EndDrawing();
+            }
+            Raylib.WindowShouldClose();
+        }
+        static void Setup()
+        {
+
+        }
+        static void Update()
+        {
+
         }
     }
 }
